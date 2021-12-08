4f18c33e42600f5343f54a01ee186c15b93bc32e
Hai Qi <53511724+0xhqli@users.noreply.github.com>
2021-12-07 16:37:50 -0800

    Update Day7.rs
    Commented out print statements used to check if data was in desired format

--- Day7/Day7.rs
+++ Day7/Day7.rs
@@ -8,11 +8,11 @@ fn main() {
         .filter(|s| !s.is_empty())
         .map(|s| s.parse().unwrap())
         .collect();
-    println!("input: {:?}",input);
+    // println!("input: {:?}",input);
     input.sort();
     let mid = input.len() / 2;
     let median:i32=input[mid];
-    println!("median: {}",median);
+    // println!("median: {}",median);
     let fuelp1: i32=input.iter().fold(0, |total,x| total+i32::abs(median-x));
     println!("p1: {}",fuelp1);
     println!("p2: {}",p2(&mut input));
@@ -21,15 +21,15 @@ fn p2(crabs: &mut Vec<i32>)-> i32{
     let mut min_fuel=i32::MAX;
     let min:i32=i32::from(*crabs.iter().min().unwrap());
     let max:i32=i32::from(*crabs.iter().max().unwrap());
-    println!("crabs: {:?}",crabs);
-    println!("min: {}",min);
-    println!("max: {}",max);
+    // println!("crabs: {:?}",crabs);
+    // println!("min: {}",min);
+    // println!("max: {}",max);
     let mut crab_locations= HashMap::new();
     for crab in crabs {
         let count = crab_locations.entry(crab).or_insert(0);
         *count += 1;
     }
-    println!("crabs locations: {:?}",crab_locations);
+    // println!("crabs locations: {:?}",crab_locations);
     for final_location in min..=max {
         let mut fuel:i32=0;
         for (start, num_crabs)in &crab_locations{
